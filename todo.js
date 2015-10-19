dce619a38981b57b7734c7b6e183331df9226ccc
johnan18 <johnan18@luther.edu>
2015-10-19 14:19:44 -0500

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,9 +1,14 @@
 changeThisPageFunc=function(){
   var input;
-  var ch;
+  
   input=document.getElementById("todo").value;
-  ch=document.querySelector('#tasklist');
-  ch.innerHTML=input;
+  var list1 = document.createElement("li");
+  var ul = document.createElement("ul");
+  list1text = document.createTextNode(input);
+  
+  document.section.appendChild(ul);
+  ul.appendChild(list1);
+  list1.appendChild(list1text);
   
 }
 
